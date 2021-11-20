94cbf67df2c6454fa8dee578a406a2569f5884bd
Daniel Whitford <whitford.dsw@gmail.com>
2021-11-20 19:29:22 +0000

    More basic operators

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,25 +1,31 @@
 package interpreter
 
 import (
+	"fmt"
+
 	"whitford.io/danlisp/expr"
 )
 
 var environment map[string]interface{}
 
-func Interpret(exprs []expr.Expr) interface{} {
+func Interpret(exprs []expr.Expr) (interface{}, error) {
 	environment = newEnvironment()
 
 	var retval interface{}
+	var err error
 	for _, ex := range exprs {
-		retval = eval(ex)
+		retval, err = eval(ex)
+		if err != nil {
+			return nil, err
+		}
 	}
-	return retval
+	return retval, nil
 }
 
-func eval(ex expr.Expr) interface{} {
+func eval(ex expr.Expr) (interface{}, error) {
 	atom, ok := ex.(expr.Atom)
 	if ok {
-		return evalAtom(atom)
+		return evalAtom(atom), nil
 	}
 
 	seq, ok := ex.(expr.Seq)
@@ -29,7 +35,7 @@ func eval(ex expr.Expr) interface{} {
 
 	symbol, ok := ex.(expr.Symbol)
 	if ok {
-		return symbol
+		return symbol, nil
 	}
 
 	panic("Don't know how to eval this thing")
@@ -39,19 +45,28 @@ func evalAtom(ex expr.Atom) interface{} {
 	return ex.Value
 }
 
-func evalSeq(ex expr.Seq) interface{} {
-	fname := eval(ex.Exprs[0]).(expr.Symbol).Name
+func evalSeq(ex expr.Seq) (interface{}, error) {
+	symbol, _ := eval(ex.Exprs[0])
+	fname := symbol.(expr.Symbol).Name
 	args := []interface{}{}
 	for _, argex := range ex.Exprs[1:] {
-		args = append(args, eval(argex))
+		arg, _ := eval(argex)
+		args = append(args, arg)
 	}
-	applyer := environment[fname].(func(argv []interface{}) interface{})
-	return applyer(args)
+	applyer, ok := environment[fname].(func(argv []interface{}) interface{})
+	if !ok {
+		return nil, fmt.Errorf("Runtime error. Could not find function '%v'.", fname)
+	}
+	return applyer(args), nil
 }
 
 func newEnvironment() map[string]interface{} {
 	env := make(map[string]interface{})
 	env["+"] = func(argv []interface{}) interface{} { return argv[0].(float64) + argv[1].(float64) }
+	env["-"] = func(argv []interface{}) interface{} { return argv[0].(float64) - argv[1].(float64) }
+	env["*"] = func(argv []interface{}) interface{} { return argv[0].(float64) * argv[1].(float64) }
+	env["/"] = func(argv []interface{}) interface{} { return argv[0].(float64) / argv[1].(float64) }
+	env["mod"] = func(argv []interface{}) interface{} { return int(argv[0].(float64)) % int(argv[1].(float64)) }
 
 	return env
 }
--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -28,18 +28,44 @@ func getExpressions(input string) []expr.Expr {
 
 func TestJustNumber(t *testing.T) {
 	exprs := getExpressions("101")
-	ret := Interpret(exprs)
+	ret, _ := Interpret(exprs)
 	assertNumber(t, 101, ret.(float64))
 }
 
 func TestJustString(t *testing.T) {
 	exprs := getExpressions("\"testing testing\"")
-	ret := Interpret(exprs)
+	ret, _ := Interpret(exprs)
 	assertString(t, "testing testing", ret.(string))
 }
 
 func TestAddTwo(t *testing.T) {
 	exprs := getExpressions("(+ 2 7)")
-	ret := Interpret(exprs)
+	ret, _ := Interpret(exprs)
 	assertNumber(t, 9, ret.(float64))
+}
+
+func TestSubtract(t *testing.T) {
+	exprs := getExpressions("(- 2 7)")
+	ret, _ := Interpret(exprs)
+	assertNumber(t, -5, ret.(float64))
+}
+
+func TestErrorFuncNotFound(t *testing.T) {
+	exprs := getExpressions("(nonsuch 2 7)")
+	_, err := Interpret(exprs)
+	assertString(t, "Runtime error. Could not find function 'nonsuch'.", err.Error())
+}
+
+func TestMoreBasicOperators(t *testing.T) {
+	exprs := getExpressions("(* 2 7)")
+	ret, _ := Interpret(exprs)
+	assertNumber(t, 14, ret.(float64))
+
+	exprs = getExpressions("(/ 10 4)")
+	ret, _ = Interpret(exprs)
+	assertNumber(t, 2.5, ret.(float64))
+
+	exprs = getExpressions("(mod 10 4)")
+	ret, _ = Interpret(exprs)
+	assertNumber(t, 2, float64(ret.(int)))
 }
